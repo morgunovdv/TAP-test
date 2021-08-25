9fa6a1f909a06ccbeed720683125b7eba6067325
morgunovdv <morgunovdv3@hotmail.com>
2021-08-25 16:24:49 +0700

    1

---
+++ Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAP_test.Models
+{
+    [Keyless]
+    public class Skill
+    {
+        public string Name { get; set; }
+        public byte Level { get; set; } //добавить ограничение по уровню 1 - 10
+    }
+}
