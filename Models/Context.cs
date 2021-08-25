ba172f122139b5ac94468c51559696886fcb56c4
morgunovdv <morgunovdv3@hotmail.com>
2021-08-25 13:53:31 +0700

    Добавьте файлы проекта.

---
+++ Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TAP_test.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAP_test.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        //public List<Skill> Skills { get; set; }
+    }
+}
